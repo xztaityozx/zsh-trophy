c5410feb03ff57003a4709c8fc1ebbda3d0898c2
xztaityozx <taityonohibi@gmail.com>
2021-04-24 13:04:39 +0900

    ロード用のスクリプト

--- script/zt.zsh
+++ script/zt.zsh
@@ -1,5 +1,13 @@
 #!/usr/bin/env zsh
 
-ZT_DIR=$(cd $(dirname $0); cd ../; pwd)
-ZT_RECORD=${ZT_RECORD_JSON:-$ZT_DIR/.record/$USER}
+ZT_DIR=$(cd $(dirname $0)/../; pwd)
 
+function _zsh-trophy-preexec() {
+  [[ ! -e "$ZT_DIR/bin/zsh-trophy" ]] && return 0
+
+  $ZT_DIR/bin/zsh-trophy --ztd=$ZT_DIR --cmd="${1}" --width="${COLUMNS:-$(tput cols)}"
+}
+
+autoload -Uz add-zsh-hook
+
+add-zsh-hook -Uz preexec _zsh-trophy-preexec
